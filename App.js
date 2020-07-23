ecb9ed06ab2ea71a518e9baebc5b066ed8c8f2a8
Cyrille Penaye <penayecyrille@hotmail.com>
2020-07-23 16:44:20 +0100

    Android support and video download functionnality

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Button, View, Text, Image, StyleSheet, TouchableOpacity, Platform, Dimensions } from 'react-native';
+import { Button, View, Text, Image, StyleSheet, TouchableOpacity, Platform, Dimensions, ScrollView, ToastAndroid, PermissionsAndroid } from 'react-native';
 import { WebView } from 'react-native-webview';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -7,11 +7,15 @@ import { createMaterialTopTabNavigator } from '@react-navigation/material-top-ta
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import { Icon } from 'react-native-elements';
+import * as FileSystem from 'expo-file-system';
+import * as Permissions from 'expo-permissions';
+import * as MediaLibrary from 'expo-media-library';
+
+const showToast = (message) => {
+	ToastAndroid.show(message, ToastAndroid.SHORT);
+};
 
 const styles = StyleSheet.create({
-  container: {
-    paddingTop: 50,
-  },
   header: {
   	backgroundColor: "#91285f",
   },
@@ -29,10 +33,11 @@ const styles = StyleSheet.create({
   videoContainer: {
   	backgroundColor: "white",
   	marginBottom: 20,
+  	height: Platform.OS !== "web" ? 260 : 270
   },
   videoImage: {
   	width: "100%",
-  	height: 260,
+  	height: 180,
   },
   videoTitle: {
   	paddingTop: 12,
@@ -51,6 +56,50 @@ const styles = StyleSheet.create({
   }
 });
 
+const downloadDocument = async (url) => {
+	if(Platform.OS !== 'web') {
+		const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL).catch(err => {console.log(err)});
+		if (status === "granted") {
+			var fileURI = FileSystem.documentDirectory + 'image.png';
+			const resp = await FileSystem.downloadAsync(url, fileURI);
+			
+			//save photo
+			try {
+				const asset = await MediaLibrary.createAssetAsync(fileURI);
+        		await MediaLibrary.createAlbumAsync("Download", asset, false);
+        		showToast("Téléchargement effectué!");
+			}
+			catch(err) {
+				showToast("La vidéo n'a pas pu être enregistrée!");
+				console.log(err);
+			}
+		}
+		else {
+			showToast("Téléchargement non autorisé!");
+		}
+	}
+	else {
+		// Here i can't use my api url because CORS is not enabled on the api servers
+		try {
+			var myUrl = "https://i.picsum.photos/id/884/536/354.jpg?hmac=CqsELRDUWEC8Iah_3JKD1oWJdgxnpBffbkDAckj95B4";
+			//var myUrl = "https://img.youtube.com/vi/lrA_9Lx-0nU/hqdefault.jpg";
+			fetch(myUrl).then(function(t) {
+			    return t.blob().then((b)=>{
+			        var a = document.createElement("a");
+			        a.href = URL.createObjectURL(b);
+			        a.setAttribute("download", "Nouveaufichier.jpg");
+			        a.click();
+			    }
+			    );
+			});
+			//alert('Téléchargement effectué!');
+		} catch (e) {
+			console.error(e);
+			alert("Le téléchargement n'a pas pu être effectué!");
+		}
+	}
+}
+
 function getDataUsingGet(callback) {
     fetch("https://api.napster.com/v2.1/tracks/top?apikey=NDBkNDRhYjAtZjRlOC00YTI2LTg0ODMtYTBmNWVmOTQ3YjQ1", {
 		"method": "GET",
@@ -68,7 +117,7 @@ function HomeScreen({ navigation }) {
   return (
     <View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
       <Button
-        title="Launch the test"
+        title="Démarrer le test"
         onPress={() => navigation.navigate('Shalom Test App')}
       />
     </View>
@@ -78,15 +127,26 @@ function HomeScreen({ navigation }) {
 function BlankScreen() {
   return (
     <View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
-      <Text>Blank page</Text>
+      <Text>Page vide</Text>
     </View>
   );
 }
 
+function MyWeb() {
+	return (
+		<View>
+			<WebView
+				source={{ uri: 'https://github.com/expo/expo' }}
+				style={{ marginTop: 24 }}
+			/>
+		</View>
+	);
+}
+
 function AudiosScreen() {
   return (
-    <View style={{overflow: "auto"}}>
-      <Text>Audio page</Text>
+    <View style={Platform.OS !== "web" ? {flex: 1, justifyContent: 'center', alignItems: 'center'} : {flex: 1, justifyContent: 'center', alignItems: 'center', overflow: 'auto'}}>
+      <Text>Gestion des audios</Text>
     </View>
   );
 }
@@ -111,51 +171,52 @@ function VideosScreen() {
    	}, []);
 
 	return (
-		<View style={{flex: 1, overflow: "auto"}}>
-			<View>
-				{
-					photos.length !== 0 ? 
-						photos.map((photo, i) => (
-							<View key={i} style={styles.videoContainer}>
-								{
-									!Platform.OS === "web" ? 
-									<WebView 
-										source={{
-											html:"<iframe height='240' scrolling='no' src={photo.embed.match(/src='([^']+)/)[1]} />"
-										}} 
-										style={{width:"100%",height:200,backgroundColor:'blue',marginTop:20}}
-									/> : 
-									<div style={{display:"block",width:"inherit",overflow:"hidden",width:"100%",height:"200px"}}>
-										<iframe style={{marginTop:"-47px", width:"-webkit-fill-available"}} height="247px" scrolling="no" src={photo.embed.match(/src='([^']+)/)[1]} />
-									</div>
-								}
-								<View style={{flex: 1, flexDirection: "row", justifyContent: "space-between"}}>
-									<View>
-										<Text style={styles.videoTitle}>{photo.title.replace(/./,m=>m.toUpperCase())}</Text>
-						      			<Text style={styles.videoSubTitle}>Shalom Tabernacle</Text>
-									</View>
-									<View>
-										<Icon
-											raised
-											reverse
-											name='download'
-											type='font-awesome'
-											color='#91285f'
-											reverseColor='white'
-											size={16}
-											onPress={() => getDataUsingGet()} 
-										/>
-									</View>
+		<ScrollView style={Platform.OS !== "web" ? {flex: 1} : {flex: 1, overflow: 'auto'}}>
+			{
+				photos.length !== 0 ? 
+					photos.map((photo, i) => (
+						<View key={i} style={styles.videoContainer}>
+							{
+								Platform.OS !== "web" ? 
+								<View style={{height: 180}}>
+									<WebView
+										source={{ uri: photo.embed.match(/src='([^']+)/)[1] }}
+										style={{marginTop: -47}}
+										javaScriptEnabled={true}
+									    domStorageEnabled={true}
+									    startInLoadingState={true}
+									/>
+								</View> : 
+								<div style={{display:"block",width:"inherit",overflow:"hidden",width:"100%",height:"200px"}}>
+									<iframe style={{marginTop:"-47px", width:"-webkit-fill-available"}} height="247px" scrolling="no" src={photo.embed.match(/src='([^']+)/)[1]} />
+								</div>
+							}
+							<View style={{flex: 1, flexDirection: "row", justifyContent: "space-between"}}>
+								<View>
+									<Text style={styles.videoTitle}>{photo.title.replace(/./,m=>m.toUpperCase())}</Text>
+					      			<Text style={styles.videoSubTitle}>Shalom Tabernacle</Text>
 								</View>
-						    </View>
-						))
-					: 
-					<View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
-						<Text style={styles.loadingText}>Chargement des vidéos en cours...</Text>
-					</View>
-				}
-			</View>						
-		</View>
+								<View>
+									<Icon
+										raised
+										reverse
+										name='download'
+										type='font-awesome'
+										color='#91285f'
+										reverseColor='white'
+										size={16}
+										onPress={() => downloadDocument(photo.thumbnail)} 
+									/>
+								</View>
+							</View>
+					    </View>
+					))
+				: 
+				<View style={{flex: 1, justifyContent: "center", alignItems: "center", marginTop: 30}}>
+					<Text style={styles.loadingText}>Chargement des vidéos en cours...</Text>
+				</View>
+			}
+		</ScrollView>
 	);
 }
 
@@ -164,7 +225,7 @@ const HomeStack = createStackNavigator();
 function HomeStackScreen() {
   return (
     <HomeStack.Navigator screenOptions={{ headerTintColor: 'white', headerStyle: styles.header }}>
-      <HomeStack.Screen name="Home" component={HomeScreen} />
+      <HomeStack.Screen name="Accueil" component={HomeScreen} />
       <HomeStack.Screen name="Shalom Test App" component={Root} />
     </HomeStack.Navigator>
   );
@@ -175,7 +236,7 @@ const SettingsStack = createStackNavigator();
 function SettingsStackScreen() {
   return (
     <SettingsStack.Navigator screenOptions={{ headerTintColor: 'white', headerStyle: styles.header }}>
-      <SettingsStack.Screen name="Blank page" component={BlankScreen} />
+      <SettingsStack.Screen name="Page vide" component={BlankScreen} />
       <SettingsStack.Screen name="Shalom Test App" component={Root} />
     </SettingsStack.Navigator>
   );
@@ -219,11 +280,11 @@ export default function App() {
             }
 
             // You can return any component that you like here!
-            return <Ionicons style={{ marginBottom: "-7px" }} name={iconName} size={size} color={color} />;
+            return <Ionicons style={{marginBottom: -5}} name={iconName} size={size} color={color} />;
           },
         })}
         tabBarOptions={{
-          style: {height: 50, marginBottom: 10},
+          style: {height: 50, paddingBottom: 5},
           activeTintColor: '#2196f3',
           inactiveTintColor: 'gray',
         }}
